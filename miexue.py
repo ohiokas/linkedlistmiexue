1315c618213ae4f186678f8ed6054d0b6a70b93b
YosionBestyMarpaung <147596895+YosionBestyMarpaung@users.noreply.github.com>
2024-03-13 14:16:35 +0700

    Update miexue.py

--- miexue.py
+++ miexue.py
@@ -1,1 +1,19 @@
+class Node:
+    def __init__(self, menu, harga):
+        self.menu = menu
+        self.harga = harga
+        self.next = None
 
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def tambah_pesanan(self, menu, harga):
+        new_node = Node(menu, harga)
+        if not self.head:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = new_node
